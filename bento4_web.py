066f0d6516cb46396ba3837a496ac61bc7076252
Xingzhao Yun <Xingzhao.Yun@dolby.com>
2021-06-08 17:49:12 +0800

    update manifest name for multiple requests

--- bento4_web.py
+++ bento4_web.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect, send_file
 import subprocess
 import logging
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -75,14 +76,14 @@ app = Flask(__name__)
 @app.route('/return-manifest-live/')
 def return_files_live():
     try:
-        return send_file('/Users/xyun/Documents/Online/online_bento4/output/dash_live/live.mpd', mimetype='text/mpd',as_attachment=True)
+        return send_file('/Users/xyun/Documents/Git/bento4_flask/output/' + live_manifest_filename, mimetype='text/mpd',as_attachment=True)
     except Exception as e:
         return str(e)
 
 @app.route('/return-manifest-vod/')
 def return_files_vod():
     try:
-        return send_file('/Users/xyun/Documents/Online/online_bento4/output/dash_vod/ondemand.mpd', mimetype='text/mpd', as_attachment=True)
+        return send_file('/Users/xyun/Documents/Git/bento4_flask/output/' + vod_manifest_filename, mimetype='text/mpd', as_attachment=True)
     except Exception as e:
         return str(e)
 
@@ -91,6 +92,15 @@ def return_files_vod():
 @app.route('/config', methods=['GET', 'POST'])
 def config():
     if request.method == "POST":
+        timestamp = datetime.now().strftime('%Y_%b_%d_%H_%M_%S')
+        request_ip = request.remote_addr
+        print(timestamp)
+        print(request_ip)
+        global vod_manifest_filename, live_manifest_filename
+        vod_manifest_filename  = request_ip.replace('.','_',3) + '_' + timestamp + '_ondemand.mpd'
+        live_manifest_filename = request_ip.replace('.','_',3) + '_' + timestamp + '_live.mpd'
+        print(vod_manifest_filename)
+        print(live_manifest_filename)
         video_tracks = request.form.getlist("video_track")
         v_set = [('frag_mp4_DASH/video/'+video_track).replace('.h264', '_f_dash.mp4').replace('.h265', '_f_dash.mp4') for video_track in video_tracks]
         video_args = ' '.join(v_set)
@@ -101,27 +111,29 @@ def config():
         audio_args = ' '.join(a_set)
 
         # subprocess.call("python " + "utils/mp4-dash.py", shell=True)
-        subprocess.call('python bento4/utils/mp4-dash.py -f -o output/dash_vod --no-media --mpd-name ' \
-            'ondemand.mpd --language-map English:eng,French:fra --profiles on-demand --no-split ' + video_args + ' ' + audio_args, shell=True)
+        subprocess.call('python bento4/utils/mp4-dash.py -f -o output --no-media --mpd-name ' \
+            + vod_manifest_filename + ' --language-map English:eng,French:fra --profiles on-demand --no-split ' + video_args + ' ' + audio_args, shell=True)
 
-        subprocess.call('python bento4/utils/mp4-dash.py -f -o output/dash_live --no-media --mpd-name ' \
-            'live.mpd --language-map English:eng,French:fra --profiles live --use-segment-template-number-padding --use-segment-timeline ' \
+        subprocess.call('python bento4/utils/mp4-dash.py -f -o output --no-media --mpd-name ' \
+            + live_manifest_filename + ' --language-map English:eng,French:fra --profiles live --use-segment-template-number-padding --use-segment-timeline ' \
             + video_args + ' ' + audio_args, shell=True)
         print(video_args)
         print(audio_args)
-        with open("output/dash_live/live.mpd", "r") as live_file:
+        with open("output/" + live_manifest_filename, "r") as live_file:
             live_content = live_file.read()
-        with open("output/dash_vod/ondemand.mpd", "r") as vod_file:
+        with open("output/" + vod_manifest_filename, "r") as vod_file:
             vod_content = vod_file.read()
         profile = request.form.get("profile", None)
         if profile!=None:
-            return render_template("downloads.html", live_content = live_content, vod_content = vod_content)
+            return render_template("downloads.html", live_content = live_content, live_manifest_filename = live_manifest_filename, \
+                vod_content = vod_content, vod_manifest_filename = vod_manifest_filename)
     return render_template("config.html")
 
 
 if __name__ == '__main__':
+    #app.run(threaded=True)
     app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(host='0.0.0.0', port=80, threaded=True)
 
 
 
